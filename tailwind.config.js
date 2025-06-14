c946cc038d4065cccf6d165baa777f9ff037c800
Anja Gollner <anjakgollner@gmail.com>
2025-06-14 20:43:37 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#161618',
+        'primary': '#E4D8BC',
+        'accent': '#A8B5A2',
+        'neutral': '#F5F5F5',
+        'text': '#1F242C',
+        'bg-dark': '#B69974',
+        'bg-light': '#FAF9F6',
+        'black': '#2B2B2B',
+        'text-hover': '#E0D3C3',
+        'grey': '#999FA6',
+        'grey-text': '#67687A',
+        'box': '#ECE8E0',
+        'beige-light': '#FEEAD8',
+        'beige-dark': '#C2B5A5',
+        'black-dark': '#141414',
+        'black-light': '#1A1A1A',
+        'text-beige-dark': '#C5AE90',
+      },
+    },
+  },
+  plugins: [],
+}
+
